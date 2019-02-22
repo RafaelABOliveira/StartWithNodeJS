2241130afe650aae50a1c5766f26e33751697b84
Rafael Oliveira <rafaelntn766@gmail.com>
2019-02-22 11:30:52 -0300

    Improving with Postman and NeDB

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,13 @@ let db = new NeDB({
 
 module.exports = (app) => {
 
-    app.get('/users', (req, res) => {
+    let route = app.route('/users');
+
+    route.get((req, res) => {
 
         db.find({}).sort({ name: 1 }).exec((err, users) => {
             if (err) {
-                console.log(`error: ${err}`);
-                res.status(400).json({
-                    error: err
-                })
+                app.utils.error.send(err, req, res);
             } else {
 
                 res.statusCode = 200;
@@ -27,14 +26,11 @@ module.exports = (app) => {
 
     });
 
-    app.post('/users', (req, res) => {
+    route.post((req, res) => {
 
         db.insert(req.body, (err, user) => {
             if (err) {
-                console.log(`error: ${err}`);
-                res.status(400).json({
-                    error: err
-                })
+                app.utils.error.send(err, req, res);
             } else {
                 res.status(200).json(user);
             }
