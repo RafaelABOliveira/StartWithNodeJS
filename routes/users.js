0c049ae822fb2fbdd6cdd3637b9b34386367847a
Rafael Oliveira <rafaelntn766@gmail.com>
2019-02-22 10:29:09 -0300

    Update for postman data and install nodemon

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,44 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+})
+
 module.exports = (app) => {
 
-app.get('/users', (req, res) => {
+    app.get('/users', (req, res) => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users: [{
-            name: 'Hcode',
-            email: 'contact@you.com',
-            id: 1
-        }]
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json')
+                res.json({
+                    users
+                });
+
+            }
+        })
+
     });
 
-});
+    app.post('/users', (req, res) => {
 
-app.get('/users/admin', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
-    res.json({
-        users: []
-    });
-
-})
+        db.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                res.status(200).json(user);
+            }
+        })
+    })
 
 };
