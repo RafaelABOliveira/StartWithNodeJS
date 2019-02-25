ec76954750da10480f7bd763bc737421914add12
Rafael Oliveira <rafaelntn766@gmail.com>
2019-02-25 11:30:45 -0300

    Finish project

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
 
--- routes/users.js
+++ routes/users.js
@@ -28,6 +28,8 @@ module.exports = (app) => {
 
     route.post((req, res) => {
 
+        if(!app.utils.validator.user(app, req, res)) return false;
+
         db.insert(req.body, (err, user) => {
             if (err) {
                 app.utils.error.send(err, req, res);
@@ -35,6 +37,41 @@ module.exports = (app) => {
                 res.status(200).json(user);
             }
         })
+    })
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+        db.findOne({_id:req.params.id}).exec((err, user) => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
+            }
+        });
+    })
+
+    routeId.put((req, res) => {
+
+        if(!app.utils.validator.user(app, req, res)) return false;
+
+        db.update({_id:req.params.id}, req.body, err => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+        });
+    })
+
+    routeId.delete((req, res) =>{
+        db.remove({_id:req.params.id}, {}, err=>{
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(req.params);
+            }
+        });
     })
 
 };
